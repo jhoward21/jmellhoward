86d0742f9d36b0cdcad556a343708dee45188195
Latasha Howard <l_howard@outlook.com>
2020-02-07 20:50:15 -0600

    JavaScript Practice.

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
-// create a variable called "name" that references a string
-const name = "Jermell";
-// create a variable called "favoriteSong" that references a string
-let favoriteSong = "LOVE by Kendrick";
-// create a variable called "wage" that references a number
-let wage = 7;
-// create a variable called "age" that references a number
-let age = 19;
-// create a variable called "onlyChild" that references a boolean
-const onlyChild = false;
-// create a variable called "satisfied" that references a boolean
-let satisfied = true;
-// create a variable called "favoriteThing" that references ANY primitive value
-let favoriteThing = "football";
-// print the data type of "favoriteThing
-console.log(typeof favoriteThing);
-// ! You heard a new song that became your favorite.
-favoriteSong = "Stars by Unknown";
-// Reassign the variable "favoriteSong"
-// ! You no longer like the type of data stored in "favoriteThing"
-// Reassign "favoriteThing" to a different primitive data type and print its type
-favoriteThing = "basketball";
-// ! Happy Birthday!
-// Reassign "age" to a new value
-age = 20;
-// ! You got a big raise at work.
-// Reassign "wage" to a new value
-wage = 100000;
-// ! Your raise changed your job satisfaction.
-// Assign "satisfied" to true
-satisfied = true;
-// use "console.log()" and "+" to print a sentence that includes two variables
+//'const' is not about IMMUTABILITY (ability to change) - it's about NO  REASSIGNMENT.
+// OBJECTS can change.  PRIMITIVES cannot.
+const me = {};
+//I CANNOT REASSIGN 'me'
+//me = "19";
+
+//I CAN reassign the VALUE of keys inside of 'me'.
+me.name = "Jermell";
+
+const someone = {
+  animal: "wolf",
+  weight: 155,
+  others: {
+    bear: "Boogie",
+    Moose: "Sam"
+  }
+};
+
+someone.animal = "wolf";
+someone.occupation = "deer";
+console.log(someone.others.bear);
