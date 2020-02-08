7f2b6474c54e36a9d92ee8da66286f89177f5388
Latasha Howard <l_howard@outlook.com>
2020-02-07 20:21:43 -0600

    New.

---
+++ index.js
@@ -0,0 +1,32 @@
+// create a variable called "name" that references a string
+const name = "Jermell";
+// create a variable called "favoriteSong" that references a string
+let favoriteSong = "LOVE by Kendrick";
+// create a variable called "wage" that references a number
+let wage = 7;
+// create a variable called "age" that references a number
+let age = 19;
+// create a variable called "onlyChild" that references a boolean
+const onlyChild = false;
+// create a variable called "satisfied" that references a boolean
+let satisfied = true;
+// create a variable called "favoriteThing" that references ANY primitive value
+let favoriteThing = "football";
+// print the data type of "favoriteThing
+console.log(typeof favoriteThing);
+// ! You heard a new song that became your favorite.
+favoriteSong = "Stars by Unknown";
+// Reassign the variable "favoriteSong"
+// ! You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = "basketball";
+// ! Happy Birthday!
+// Reassign "age" to a new value
+age = 20;
+// ! You got a big raise at work.
+// Reassign "wage" to a new value
+wage = 100000;
+// ! Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisfied = true;
+// use "console.log()" and "+" to print a sentence that includes two variables
